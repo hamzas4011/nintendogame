5f2cc5fada50e3d09ad77077f5d7ac11c25d7982
fazal <fazal.123@hotmail.com>
2020-11-29 02:49:12 +0100

    fikset legg til bilde

--- src/components/Console.js
+++ src/components/Console.js
@@ -5,7 +5,7 @@ const Console = ( { id, name, year, price, image}) => {
             <h3>{ name } (id: { id }) </h3>
             <p>År: { year } </p>
             <p>Price: { price }</p>
-            <img asrc={`https://localhost:5001/images/${image}`}/>
+            <img src={`https://localhost:5001/images/${image}`}/>
         </article>
     )
 }
--- src/views/AllGamesAdminAdd.js
+++ src/views/AllGamesAdminAdd.js
@@ -1,15 +1,51 @@
-
-import React from 'react';
-
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import { useState } from 'react';
+import axios from 'axios';
 const AllGamesAdminAdd = () => {
+
+    const [ name, setName ] = useState("F");
+    const [ category, setCategory ] = useState("A");
+    const [ price, setPrice ] = useState("Z");
+    const [ image, setImage ] = useState("L")
+
+    const handleChange = ( e ) => {
+        switch( e.target.id ){
+            case "name":
+                setName( e.target.value );
+                break;
+            case "category":
+                setCategory( e.target.value );
+                break;
+            case "price":
+                setPrice(parseInt ( e.target.value ));
+                break;
+            case "image":
+                setImage( e.target.value );
+                break;
+            default:
+
+
+        }
+    }
+
+    const AllGamesAdminAdd = () => {
+        const url = "https://localhost:5001/games";
+        const newGame = { name: name, category: category, price: price, image: image };
+
+        axios.post(url, newGame)
+    }
 
     return (
         <section>
-            <h3>Legg til spill admin</h3>
-            
+            <h3>Skap nytt Spill</h3>
+            <label>Navn</label>
+            <input id="name" onChange={ handleChange } type="text" value={ name }/>
+            <label>Kategori</label>
+            <input id="category" onChange={ handleChange } type="text" value={ category }/>
+            <label>Pris</label>  
+            <input id="price" onChange={ handleChange } type="number" value={ price }/>
+            <label>Bilde</label>
+            <input id="image" onChange={ handleChange }type="text" value={ image }/>
+            <input onClick={ AllGamesAdminAdd } type="button" value="Lagre nytt spill"></input> 
         </section>
     )
 }
