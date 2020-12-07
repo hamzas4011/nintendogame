7454f634aa2c352199dc126f06c431450b172e21
fazal <fazal.123@hotmail.com>
2020-12-07 19:27:57 +0100

    game fiksa update

--- src/components/game/GameUpdate.js
+++ src/components/game/GameUpdate.js
@@ -1,7 +1,6 @@
 import { GameContext } from '../../contexts/GameContext';
 import { useContext } from 'react';
 import axios from 'axios';
-
 const GameUpdate = () => {
 
     const { game, games } = useContext( GameContext);
@@ -27,7 +26,19 @@ const GameUpdate = () => {
             <p>Klikk først på Oppdater-knappen til et av komponentene under, deretter kan du endre på navnet på spillet.</p>
             <label>Navn</label>
             <input 
-            onChange={ (e) => setGame( {...gameState, name: e.target.value } ) } type="text" value={ gameState.name }></input>
+            onChange={(e) => setGame( {...gameState, name: e.target.value } ) } type="text" value={ gameState.name }></input>
+
+            <label>Bilde</label>
+            <input 
+            onChange={ (e) => setGame( {...gameState, image: e.target.value } ) } type="text" value={ gameState.image }></input>
+
+            <label>kategori</label>
+            <input 
+            onChange={ (e) => setGame( {...gameState, category :(e.target.value) } ) } type="text" value={ gameState.category }></input>
+
+           <label>Pris</label>
+            <input 
+            onChange={ (e) => setGame( {...gameState, price : e.target.value } ) } type="number" value={ gameState.price }></input>
             <input onClick={ updateGame } type="button" value="Endre"></input>
         </section>
     )
